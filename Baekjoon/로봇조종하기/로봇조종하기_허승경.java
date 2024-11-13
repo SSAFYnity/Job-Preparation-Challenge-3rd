ae1775694fac6f73215c7bbbbf5d85e4a9eb43a3
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-13 14:33:21 +0000

    unsolved 로봇조종하기

--- Baekjoon/로봇조종하기/로봇조종하기_허승경.java
+++ Baekjoon/로봇조종하기/로봇조종하기_허승경.java
@@ -1,50 +1,46 @@
+package Nov_2024;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int [] dx = {0, 1, 0};
-    static int [] dy = {-1, 0, 1};
-    static int [][] arr;
-    static long [][] nums;
-    static int n;
-    static int m;
-    static long sum;
+public class BOJ_2169_1113 {
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][m];
-        nums = new long[n][m];
-        sum = 0;
+        int [][] arr = new int[n][m];
+        long [][] mars = new long[n+1][m+1];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        nums[0][0] = arr[0][0];
-        dfs(0, 0);
 
-        System.out.println(sum);
-    }
+        // 0: 왼, 1:아래, 2: 오
+        mars[1][1] = arr[0][0];
 
-    static void dfs(int x, int y) {
-        if(x == n-1 && y == m-1){
-            sum = Math.max(sum, nums[x][y]);
-            return;
+        int [] dx = {0, -1, 0};  // 왼, 아래, 오
+        int [] dy = {-1, 0, 1};
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                long tmp = 0;
+                for(int k = 0; k < 3; k++){
+                    long tmp_sum = 0;
+                    int tx = i + dx[k];
+                    int ty = j + dy[k];
+
+                    if(tx < 1 || tx > n || ty < 1 || ty > m) continue;
+                    tmp_sum += mars[tx][ty] + arr[i-1][j-1];
+                    tmp = Math.max(tmp_sum, tmp);
+
+                }
+                mars[i][j] = Math.max(tmp, mars[i][j]);
+            }
         }
 
-        for(int i = 0; i < 3; i++) {
-            int tx = x + dx[i];
-            int ty = y + dy[i];
-
-            if (tx < 0 || tx >= n || ty < 0 || ty >= n) continue;
-            long tmp = nums[tx][ty];
-            nums[tx][ty] = nums[x][y] + arr[tx][ty];
-            dfs(x, y);
-            nums[tx][ty] = tmp;
-        }
+        System.out.println(mars[n][m]);
     }
 }
