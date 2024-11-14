003e78000b8330a07541cc65940fa87b250e7ee6
skyungg <167261107+skyungg@users.noreply.github.com>
2024-11-14 11:13:32 +0000

    solved 스타수열 - 156.32ms 147mb

--- Programmers/스타수열/스타수열_허승경.java
+++ Programmers/스타수열/스타수열_허승경.java
@@ -1,65 +1,30 @@
 import java.util.*;
 
 class Solution {
-    int [] a;
-    int maxLength = -1;
-    
     public int solution(int[] a) {
-        this.a = a;
-        int n = a.length;
-        if(n < 2) return 0;
-        if(a.length % 2  == 1) n -= 1;
+        if(a.length < 2) return 0;
         
-        // 조합 만들기
-        for(int i = n; i > 0; i = i-2){
-            // 짝수개의 조합 만들기(최대 길이부터)
-            makeCombination(i, new int[i], 0, 0);
-            
-            if(maxLength != -1) break;
+        // 배열 속 숫자의 빈도 -> map에 저장
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : a){
+            map.put(n, map.getOrDefault(n, 0)+1);
         }
         
-        if(maxLength == -1) maxLength = 0;
+        int maxLength = 0;  // 최대 길이
+        for(int key : map.keySet()){
+            if (map.get(key) * 2 <= maxLength) continue;
+            
+            int pairs = 0;
+            
+            for(int i = 0; i < a.length-1; i++){
+                if((a[i] == key || a[i+1] == key) && a[i] != a[i+1]){
+                    pairs++;
+                    i++;
+                }
+            }
+            maxLength = Math.max(maxLength, pairs*2);
+        }
         
         return maxLength;
-    }
-    
-    void makeCombination(int depth, int [] combi, int start, int idx){
-        if(depth == idx){
-            // 조합 생성 -> 스타배열 확인하기
-            int [] res = new int[depth];
-            for(int i = 0; i < combi.length; i++){
-                res[i] = a[combi[i]];
-            }
-            checkSequence(res);
-            return;
-        }
-        
-        for(int i = start; i < a.length; i++){
-            combi[idx] = i;     // 인덱스 저장
-            makeCombination(depth, combi, i+1, idx+1);
-            if(maxLength != -1) break;
-        }
-        
-    }
-    void checkSequence(int [] comb){
-        Map<Integer, Integer> elementCount = new HashMap<>();
-
-        for (int i = 0; i < comb.length - 1; i += 2) {
-            int x = comb[i];
-            int y = comb[i + 1];
-
-            if (x == y) return; // 서로 다른 두 요소로 이루어지지 않으면 반환
-
-            elementCount.put(x, elementCount.getOrDefault(x, 0) + 1);
-            elementCount.put(y, elementCount.getOrDefault(y, 0) + 1);
-        }
-
-        // 공통된 요소가 하나 이상 존재하는지 확인
-        for (int count : elementCount.values()) {
-            if (count >= comb.length / 2) {
-                maxLength = Math.max(maxLength, comb.length);
-                return;
-            }
-        }
     }
 }
