f9cdba02fda97e7603d55262322984e2711070fe
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2024-11-18 20:08:45 +0900

    solved 소수 구하기 - 116ms 14452KB

---
+++ Baekjoon/소수구하기/소수구하기_강정훈.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        boolean[] check = new boolean[N+1];
+        check[1] = true;
+        for (int index = 2; index <= Math.sqrt(N); index++) {
+            int target = index*2;
+            while(target < N+1) {
+                check[target] = true;
+                target += index;
+            }
+        }
+        for (int index = M; index < N+1; index++) {
+            if(!check[index]) {
+                System.out.println(index);
+            }
+        }
+    }
+}
